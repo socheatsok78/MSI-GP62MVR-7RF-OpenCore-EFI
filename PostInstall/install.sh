5fbeb23de0a040b9ef71daadffa27806105fa2f8
Socheat Sok <alex@socheat.net>
2020-02-05 09:50:23 +0700

    Fix PostInstall script

--- PostInstall/install.sh
+++ PostInstall/install.sh
@@ -6,13 +6,23 @@ set -e
 # https://www.tonymacx86.com/threads/stop-the-popping-in-high-sierra.202355/
 
 intallAudioService() {
-    local INSTALL_TARGET="/Library/LaunchDaemons"
-    local AUDIO_SERVICE="./Library/LaunchDaemons/com.msi.audio.plist"
+    local INSTALL_TARGET="/Library/LaunchDaemons/com.msi.audio.plist"
+    local AUDIO_SERVICE_NAME="com.msi.audio.plist"
+    local AUDIO_SERVICE="Library/LaunchDaemons/$AUDIO_SERVICE_NAME"
 
-    sudo cp "$AUDIO_SERVICE" "$INSTALL_TARGET"
+    if [ -f "$INSTALL_TARGET" ]; then
+        echo -n "Removing existing service..."
+        sudo rm "$INSTALL_TARGET" >>/dev/null 2>&1
+        echo " [Done]"
+    fi
 
-    if [ -f "$AUDIO_SERVICE" ]; then
-        sudo launchctl load "$AUDIO_SERVICE"
+    echo -n "Installing new service..."
+    sudo cp "$AUDIO_SERVICE" "$INSTALL_TARGET" >>/dev/null 2>&1
+    echo " [Done]"
+
+    if [ -f "$INSTALL_TARGET" ]; then
+        echo "Registering new service..."
+        sudo launchctl load "$INSTALL_TARGET"
     fi
 
     return 1
