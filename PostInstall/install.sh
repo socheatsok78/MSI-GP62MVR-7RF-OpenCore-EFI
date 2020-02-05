3c504d26a923b14edfd1fefc3298347e10895938
Socheat Sok <alex@socheat.net>
2020-02-05 09:57:08 +0700

    Update PostInstall script, ask sudo upfront and improve service

--- PostInstall/install.sh
+++ PostInstall/install.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 set -e
+sudo -v # ask for sudo upfront
 
 # Stop popping sound produce by Audio driver
 # https://www.tonymacx86.com/threads/stop-the-popping-in-high-sierra.202355/
+
+# !!Do not remove main function!!
+main() {
+    intallAudioService
+}
 
 intallAudioService() {
     local INSTALL_TARGET="/Library/LaunchDaemons/com.msi.audio.plist"
@@ -12,24 +18,21 @@ intallAudioService() {
 
     if [ -f "$INSTALL_TARGET" ]; then
         echo -n "Removing existing service..."
-        sudo rm "$INSTALL_TARGET" >>/dev/null 2>&1
+        launchctl unload "$INSTALL_TARGET"
+        rm "$INSTALL_TARGET" >>/dev/null 2>&1
         echo " [Done]"
     fi
 
     echo -n "Installing new service..."
-    sudo cp "$AUDIO_SERVICE" "$INSTALL_TARGET" >>/dev/null 2>&1
+    cp "$AUDIO_SERVICE" "$INSTALL_TARGET" >>/dev/null 2>&1
     echo " [Done]"
 
     if [ -f "$INSTALL_TARGET" ]; then
         echo "Registering new service..."
-        sudo launchctl load "$INSTALL_TARGET"
+        launchctl load "$INSTALL_TARGET"
     fi
 
     return 1
-}
-
-main() {
-    intallAudioService
 }
 
 # Execute Main
